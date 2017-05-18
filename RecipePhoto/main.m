9fe1421d5a6b35d6b13a7e5bb3ec20c5c8abb337
Schnorts <Radbonus@apples-MacBook-Pro.local>
2017-05-18 15:42:31 +0200

    Initial Commit

---
+++ RecipePhoto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecipePhoto
+//
+//  Created by Radbonus on 18.05.17.
+//  Copyright © 2017 Radbonus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
